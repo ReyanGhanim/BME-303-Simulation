b4a9432683eb167f0168413d3200e9de80595d60
sahir-ahmed42 <sahir.ahmed@utexas.edu>
2024-04-02 16:02:08 -0500

    working mesh

--- Main.py
+++ Main.py
@@ -3,5 +3,12 @@ import time
 
 rows = 40
 columns = 40
-matrix = []
+mesh = []
 for i in range(rows):
+    row = []
+    for j in range(columns):
+        row.append(0);
+    mesh.append(row)
+print(mesh)
+mesh[0][2] = 5
+print(mesh)
