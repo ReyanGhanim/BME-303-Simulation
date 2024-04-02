18ec65c6447a3a64ab8a4f7f912ae0d0403471eb
sahir-ahmed42 <sahir.ahmed@utexas.edu>
2024-04-02 16:06:35 -0500

    Sahir file

---
+++ SahirMesh.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+rows = 40
+columns = 40
+mesh = []
+for i in range(rows):
+    row = []
+    for j in range(columns):
+        row.append(0);
+    mesh.append(row)
+print(mesh)
+mesh[0][2] = 5
+print(mesh)
